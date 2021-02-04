b26e0c5badf8f4bd1483e871d24dbd1bffd0e8cd
Dongkeun.YOON <31213371+Bronzeroot@users.noreply.github.com>
2021-02-05 08:33:56 +0900

    Update 지도학습알고리즘.py

--- mlBasic/지도학습알고리즘.py
+++ mlBasic/지도학습알고리즘.py
@@ -33,10 +33,31 @@ print("CharacterName \n{}".format(cancer.feature_names))
 from sklearn.datasets import load_boston
 boston = load_boston()
 print("data shape : \n{}".format(boston.data.shape))
-X, y = mglearn.datasets.load_extended_boston()
-print("X.shape: {}".format(X.shape))
 
 
 #knn 예시
 mglearn.plots.plot_knn_classification(n_neighbors=1)
 mglearn.plots.plot_knn_classification(n_neighbors=3)
+
+
+#K-최근접 이웃 
+
+X, y = mglearn.datasets.load_extended_boston()
+print("X.shape: {}".format(X.shape))
+
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors=3)
+clf.fit(X_train,y_train)
+print("Test set predict:{}".format(clf.predict(X_test)))
+print("Test set Accuracy {:.2f}".format(clf.score(X_test, y_test)))
+
+fig, axes = plt.subplots(1,3,figsize=(10,3))
+
+for n_neighbors, ax in zip([1,3,9],axes):
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X,y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4)
+    mglearn.discrete_scatter(X[:,0],X[:,1],y,ax=ax)
+    ax.set_title("{} neighbor".format(n_neighbors))
+    ax.set_xlabel("Character 0")
+    ax.set_ylabel("Character 1")
+axes[0].legend(loc=3)
